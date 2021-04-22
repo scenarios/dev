2a663c0eabcf94ecba05a9958dcc11658e0ef777
Yizhou Zhou <zyz0205@hotmail.com>
2021-04-22 14:25:21 +0800

    export interfance for mixed recognizer

--- mmaction/models/recognizers/__init__.py
+++ mmaction/models/recognizers/__init__.py
@@ -4,3 +4,7 @@ from .recognizer2d import Recognizer2D
 from .recognizer3d import Recognizer3D
 
 __all__ = ['BaseRecognizer', 'Recognizer2D', 'Recognizer3D', 'AudioRecognizer']
+
+from .mixed_recognizer import MixRecognizer2D
+
+__all__.append('MixRecognizer2D')
