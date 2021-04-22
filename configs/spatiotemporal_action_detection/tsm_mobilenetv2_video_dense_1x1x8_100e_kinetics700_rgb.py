f699234a07078d14698a25a63c8d783afddb813b
Yizhou Zhou <zyz0205@hotmail.com>
2021-04-22 23:07:45 +0800

    [DEBUG] correct validation file path and number of video class

--- configs/spatiotemporal_action_detection/tsm_mobilenetv2_video_dense_1x1x8_100e_kinetics700_rgb.py
+++ configs/spatiotemporal_action_detection/tsm_mobilenetv2_video_dense_1x1x8_100e_kinetics700_rgb.py
@@ -1,16 +1,37 @@
 _base_ = [
-    '../../_base_/models/tsm_mobilenet_v2.py',
-    '../../_base_/schedules/sgd_tsm_mobilenet_v2_100e.py',
-    '../../_base_/default_runtime.py'
+    '../_base_/schedules/sgd_tsm_mobilenet_v2_100e.py',
+    '../_base_/default_runtime.py'
 ]
+
+model = dict(
+    type='MixRecognizer2D',
+    backbone=dict(
+        type='MobileNetV2TSM',
+        shift_div=8,
+        num_segments=8,
+        is_shift=True,
+        pretrained=None),
+    cls_head=dict(
+        type='TSMHead',
+        num_segments=8,
+        num_classes=700,
+        in_channels=1280,
+        spatial_type='avg',
+        consensus=dict(type='AvgConsensus', dim=1),
+        dropout_ratio=0.5,
+        init_std=0.001,
+        is_shift=True),
+    # model training and testing settings
+    train_cfg=None,
+    test_cfg=dict(average_clips='prob'))
 
 # dataset settings
 dataset_type = 'WeVideoDataset'
 data_root = '/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020'
 data_root_val = '/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics-700-2020'
 ann_file_train = '/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700_list/train_reorged.csv'
-ann_file_val = '/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700_list/val_reorged.csv'
-ann_file_test = '/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700_list/val_reorged.csv'
+ann_file_val = '/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700_list/validate_reorged.csv'
+ann_file_test = '/mnt/wfs/mmcommwfssz/project_mm-base-vision/harryizzhou/projects/video_understanding/data/kinetics700_list/validate_reorged.csv'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_bgr=False)
 train_pipeline = [
@@ -69,18 +90,25 @@ data = dict(
     train=dict(
         type=dataset_type,
         ann_file=ann_file_train,
+        exclude_file=None,
         data_prefix=data_root,
-        pipeline=train_pipeline),
+        pipeline=train_pipeline,
+        num_video_classes=701),
     val=dict(
         type=dataset_type,
         ann_file=ann_file_val,
+        exclude_file=None,
         data_prefix=data_root_val,
-        pipeline=val_pipeline),
+        pipeline=val_pipeline,
+        num_video_classes=701),
     test=dict(
         type=dataset_type,
         ann_file=ann_file_test,
+        exclude_file=None,
         data_prefix=data_root_val,
-        pipeline=test_pipeline))
+        pipeline=test_pipeline,
+        num_video_classes=701))
+
 evaluation = dict(
     interval=5, metrics=['top_k_accuracy', 'mean_class_accuracy'])
 
@@ -91,4 +119,4 @@ optimizer = dict(
 
 # runtime settings
 checkpoint_config = dict(interval=5)
-work_dir = './work_dirs/tsm_mobilenetv2_dense_video_1x1x8_100e_kinetics400_rgb/'  # noqa
+work_dir = './work_dirs/tsm_mobilenetv2_dense_video_1x1x8_100e_kinetics700_rgb/'  # noqa
